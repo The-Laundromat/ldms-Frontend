034b7b825502dfb70e0d2a3441b7903bcac12b0d
Lahiru Gayantha <lahirugayantha@yaho.com>
2022-03-07 21:27:25 +0530

    User general detail page has designed.

--- src/app/core/auth/auth.component.ts
+++ src/app/core/auth/auth.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "./auth.service";
-import {Router, Routes} from "@angular/router";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-auth',
